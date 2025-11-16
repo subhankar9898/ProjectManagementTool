799f63dd2c63699f8ed497e46121264a4b217d76
Subhankar Prusty <subhankar99p@gmail.com>
2025-11-17 00:10:12 +0530

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -2,7 +2,6 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv() # This loads the variables from your .env file
-from app import app
 from flask import Flask, request, jsonify
 from datetime import datetime
 from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
